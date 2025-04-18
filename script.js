47af924d7d725496220b6027166509cdcd77426a
Thomas O'Connor <thomas.Ocono@gmail.com>
2025-04-18 07:29:22 -0400

    x-axis working

--- script.js
+++ script.js
@@ -8,7 +8,8 @@ let width = 1000;
 let padding = 50;
 
 const main = (data) => {
-  d3.select("body")
+  const svg = d3
+    .select("body")
     .append("svg")
     .attr("height", height)
     .attr("width", width)
@@ -16,7 +17,35 @@ const main = (data) => {
     .style("display", "block")
     .style("margin", "auto");
 
-  console.log();
+  let timeArray = data.map((d) => d.Time);
+  let yearsArray = data.map((d) => d.Year);
+
+  const toSeconds = (time) => {
+    const [min, sec] = time.split(":");
+    return parseInt(min) * 60 + parseInt(sec);
+  };
+  let secondsArray = timeArray.map((d) => toSeconds(d));
+
+  let xScale = d3
+    .scaleLinear()
+    .domain([d3.min(yearsArray), d3.max(yearsArray)])
+    .range([padding, width - padding]);
+  let yScale = d3
+    .scaleLinear()
+    .domain([d3.min(secondsArray), d3.min(secondsArray)])
+    .range([height - padding, padding]);
+
+  let xAxis = d3.axisBottom(xScale).tickFormat(d3.format("d"));
+  let yAxis = d3.axisLeft(yScale).tickFormat(d3.format("d"));
+
+  svg
+    .append("g")
+    .call(xAxis)
+    .attr("transform", "translate(0, " + (height - padding) + ")");
+  svg
+    .append("g")
+    .call(yAxis)
+    .attr("transform", "translate(" + padding + ", 0)");
 };
 
 //to get the data, then runs main function
