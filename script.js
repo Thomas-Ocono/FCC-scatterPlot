ddefd3b536909c70a223a70092f6824eaec2ac66
Thomas O'Connor <thomas.Ocono@gmail.com>
2025-04-19 20:59:05 -0400

    axis are working!

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let url =
 let req = new XMLHttpRequest();
 
 let data;
-let height = 500;
+let height = 750;
 let width = 1000;
 let padding = 50;
 
@@ -17,38 +17,41 @@ const main = (data) => {
     .style("display", "block")
     .style("margin", "auto");
 
-  let timeArray = data.map((d) => d.Time);
   let yearsArray = data.map((d) => d.Year);
 
-  const toSeconds = (time) => {
-    const [min, sec] = time.split(":");
-
-    return parseInt(min) * 60 + parseInt(sec);
-  };
-  let secondsArray = timeArray.map((d) => toSeconds(d));
-
+  //creating x scale and axis
   let xScale = d3
     .scaleLinear()
-    .domain([d3.min(yearsArray), d3.max(yearsArray)])
+    .domain([d3.min(yearsArray) - 1, d3.max(yearsArray)])
     .range([padding, width - padding]);
-  let yScale = d3
-    .scaleLinear()
-    .domain([d3.min(secondsArray), d3.min(secondsArray)])
-    .range([height - padding, padding]);
 
   let xAxis = d3.axisBottom(xScale).tickFormat(d3.format("d"));
-  let yAxis = d3.axisLeft(yScale).tickFormat(d3.format("d"));
 
   svg
     .append("g")
     .call(xAxis)
     .attr("transform", "translate(0, " + (height - padding) + ")")
     .attr("id", "x-axis");
+
+  //creating y scale and axis
+  //get times from data, then convert them into date objects
+  const timesArray = data.map((d) => d.Time);
+  const parseTime = d3.timeParse("%M:%S");
+  const parsedArray = timesArray.map((d) => parseTime(d));
+
+  let yScale = d3
+    .scaleTime()
+    .domain([d3.max(parsedArray), d3.min(parsedArray)])
+    .range([height - padding, padding]);
+  let yAxis = d3.axisLeft(yScale).tickFormat(d3.timeFormat("%M:%S"));
+
   svg
     .append("g")
     .call(yAxis)
     .attr("transform", "translate(" + padding + ", 0)")
     .attr("id", "y-axis");
+
+  //making the circles on the graph
   svg
     .selectAll("circle")
     .data(data)
@@ -56,10 +59,10 @@ const main = (data) => {
     .append("circle")
     .attr("class", "dot")
     .attr("data-xvalue", (d) => d.Year)
-    .attr("data-yvalue", (d) => d.Time)
+    .attr("data-yvalue", (d) => parseTime(d.Time))
     .attr("r", 5)
     .attr("cx", (d) => xScale(d.Year))
-    .attr("cy", (d) => yScale(toSeconds(d.Time)));
+    .attr("cy", (d) => yScale(parseTime(d.Time)));
 };
 
 //to get the data, then runs main function
